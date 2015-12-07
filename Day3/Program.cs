3ffec2912df02a88df89da5ea29fcfecf88b31f6
Benjamin Russell <benrr101@outlook.com>
2015-12-06 19:00:18 -0800

    Day 3 part 2!

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -6,6 +6,8 @@ namespace Day3
 {
     class Program
     {
+        static HashSet<Tuple<int, int>> visitedPositions = new HashSet<Tuple<int, int>>();
+
         static string ReadInputAtOnce(string fileName)
         {
             FileStream file = File.OpenRead(fileName);
@@ -17,49 +19,29 @@ namespace Day3
         {
             // Read in the input
             string input = ReadInputAtOnce("input.txt");
-            Tuple<int, int> currentPosition = new Tuple<int, int>(0, 0);
-            HashSet<Tuple<int, int>> visitedPositions = new HashSet<Tuple<int, int>> {currentPosition};
-
-            Console.WriteLine("Starting at {0}", OutputPosition(currentPosition));
+            Santa regularSanta = new Santa();
+            Santa roboSanta = new Santa();
+            visitedPositions.Add(new Tuple<int, int>(0,0));
 
             // Start reading the input
-            foreach (char direction in input)
+            for (int i = 0; i < input.Length; i++)
             {
+                char direction = input[i];
+                
                 // Determine which position to navigate to
-                Tuple<int, int> nextPosition;
-                switch (direction)
+                Santa currentSanta;
+                if (i%2 == 0)
                 {
-                    case '^':
-                        nextPosition = new Tuple<int, int>(currentPosition.Item1, currentPosition.Item2 + 1);
-                        Console.Write("N --> {0}", OutputPosition(nextPosition));
-                        break;
-                    case 'v':
-                        nextPosition = new Tuple<int, int>(currentPosition.Item1, currentPosition.Item2 - 1);
-                        Console.Write("S --> {0}", OutputPosition(nextPosition));
-                        break;
-                    case '>':
-                        nextPosition = new Tuple<int, int>(currentPosition.Item1 + 1, currentPosition.Item2);
-                        Console.Write("E --> {0}", OutputPosition(nextPosition));
-                        break;
-                    case '<':
-                        nextPosition = new Tuple<int, int>(currentPosition.Item1 - 1, currentPosition.Item2);
-                        Console.Write("W --> {0}", OutputPosition(nextPosition));
-                        break;
-                    default:
-                        Console.WriteLine("Invalid character {0}", direction);
-                        continue;
-                }
-
-                // Have we been to the next position yet?
-                if (!visitedPositions.Add(nextPosition))
-                {
-                    Console.WriteLine(" -- NEW POSITION");
+                    Console.Write("Regu:");
+                    currentSanta = regularSanta;
                 }
                 else
                 {
-                    Console.WriteLine();
+                    Console.Write("Robo:");
+                    currentSanta = roboSanta;
                 }
-                currentPosition = nextPosition;
+
+                currentSanta.UpdatePosition(direction);
             }
 
             Console.WriteLine("---------------------------");
@@ -67,9 +49,62 @@ namespace Day3
             Console.ReadLine();
         }
 
+        
+
         private static string OutputPosition(Tuple<int, int> position)
         {
             return String.Format("({0},{1})", position.Item1, position.Item2);
+        }
+
+        private class Santa
+        {
+            private Tuple<int, int> currentPosition;
+
+            public Santa()
+            {
+                currentPosition = new Tuple<int, int>(0,0);
+            }
+
+            public void UpdatePosition(char direction)
+            {
+                // Calculate the next position based on the 
+                Tuple<int, int> nextPosition;
+                switch (direction)
+                {
+                    case '^':
+                        nextPosition = new Tuple<int, int>(currentPosition.Item1, currentPosition.Item2 + 1);
+                        Console.Write(" N --> {0}", OutputPosition(nextPosition));
+                        break;
+                    case 'v':
+                        nextPosition = new Tuple<int, int>(currentPosition.Item1, currentPosition.Item2 - 1);
+                        Console.Write(" S --> {0}", OutputPosition(nextPosition));
+                        break;
+                    case '>':
+                        nextPosition = new Tuple<int, int>(currentPosition.Item1 + 1, currentPosition.Item2);
+                        Console.Write(" E --> {0}", OutputPosition(nextPosition));
+                        break;
+                    case '<':
+                        nextPosition = new Tuple<int, int>(currentPosition.Item1 - 1, currentPosition.Item2);
+                        Console.Write(" W --> {0}", OutputPosition(nextPosition));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid character {0}", direction);
+                        return;
+                }
+
+                // Add the position to the list of visited positions
+                // Have we been to the next position yet?
+                if (visitedPositions.Add(nextPosition))
+                {
+                    Console.WriteLine(" -- NEW POSITION");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+
+                currentPosition = nextPosition;
+            }
         }
     }
 }
