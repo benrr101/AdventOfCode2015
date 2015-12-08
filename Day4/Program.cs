5196ed8a366e45e16b6e8c572e7a65fd39e4d0de
Benjamin Russell <benrr101@outlook.com>
2015-12-08 11:09:59 -0800

    Day 4, MD5 hashes everywhere!

---
+++ Day4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string input = "iwrupvqb";
+            string successfulValue = null;
+            long testNumber = 0;
+            while (successfulValue == null)
+            {
+                string testValue = input + testNumber;
+                Console.WriteLine("Testing {0}", testValue);
+                string testHash = CalculateHash(testValue);
+                if (testHash.StartsWith("000000"))
+                {
+                    successfulValue = testValue;
+                }
+                testNumber++;
+            }
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Successful value is: {0}", successfulValue);
+            Console.ReadLine();
+        }
+
+        static string CalculateHash(string input)
+        {
+            MD5 hasher = new MD5Cng();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hash = hasher.ComputeHash(inputBytes);
+            
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in hash)
+            {
+                sb.Append(b.ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
